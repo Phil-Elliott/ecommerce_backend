636d0c165623d3459bbac9b3eb2e302c2492f5d9
Phil <phillippelliott@gmail.com>
2023-05-30 07:05:32 +0700

    changed protect route to use cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -89,11 +89,10 @@ export const logout = (req, res) => {
 
 export const protect = catchAsync(async (req, res, next) => {
   let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
+
+  // Check if the jwt cookie exists
+  if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
 
   if (!token) {
