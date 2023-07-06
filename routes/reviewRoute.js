985229b65deb784dfabc4538456db8414aaef37a
Phil <phillippelliott@gmail.com>
2023-07-06 16:13:57 +0700

    created reviews routes

---
+++ routes/reviewRoute.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getAllReviews,
+  getReview,
+  createReview,
+  updateReview,
+  deleteReview,
+} from "../controllers/reviewController.js";
+
+const router = express.Router();
+
+router.route("/").get(getAllReviews).post(createReview);
+
+router.route("/:id").get(getReview).put(updateReview).delete(deleteReview);
+
+export default router;
